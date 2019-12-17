c6d2de13246af3f3a1fca08af0517f681872b822
Haroon Rafique <haroon.rafique@protonmail.com>
2019-12-17 14:52:21 +0100

    02: NoSC scan with outputs added

---
+++ 02_NoSC/01_Original_04/00_Master/clean_run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Pickle and output files
+rm -r output
+rm -r bunch_output
+rm -r lost
+rm -r input
+
+rm *.tfs
+rm PTC-PyORBIT_flat_file.flt
+rm tunespread.dat
+rm madx.ps
+
+. clean_junk.sh
